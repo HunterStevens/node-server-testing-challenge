64dd04c8dfc8fc15736f37a8fad1accee6d6d500
Hunter Stevens <hunterstevens1996@gmail.com>
2020-05-21 15:14:22 -0600

    books Router completed

--- books/booksModel.js
+++ books/booksModel.js
@@ -2,10 +2,25 @@ const db = require('../data/dbConfig');
 
 module.exports={
     getAll,
+    getById,
     addBook,
     deleteBook
 }
 
 function getAll(){
-    return db('books').
+    return db('books');
+}
+function getById(id){
+    return db('books').where({id});
+}
+
+function addBook(newBook){
+    return db('books').insert(newBook)
+    .then(([newId]) =>{
+        return getById(newId);
+    });
+}
+
+function deleteBook(id){
+    return db('books').where({id}).del();
 }
